997a10b4990f191e858483f6d9890a300744438c
JayO <bluejayvrstudio@gmail.com>
2024-12-20 08:33:06 -0600

    update

--- controllers/line_following/line_following.py
+++ controllers/line_following/line_following.py
@@ -6,6 +6,7 @@ import numpy as np
 robot = Robot()
 # get timestep
 timestep = int(robot.getBasicTimeStep())
+delta_t = timestep / 1000.0
 
 # handle to e-puck wheels
 motor_left = robot.getDevice('left wheel motor')
@@ -32,13 +33,19 @@ MAX_SPEED = np.pi / 2
 phildot = 0
 phirdot = 0
 
+# odometry
+r = 0.0201
+d = 0.052
+delta_x = 0
+delta_w = 0
+
 while robot.step(timestep) != -1:
     # read ground sensors
     gs_values = []
     for gs_val in gs:
         gs_values.append(gs_val.getValue())
 
-    print(gs_values)
+    # print(gs_values)
 
     # go straight
     if gs_values[0] > 500 and gs_values[1] < 350 and gs_values[2] > 500:
@@ -52,6 +59,12 @@ while robot.step(timestep) != -1:
     elif gs_values[2] < 350:
         phildot = MAX_SPEED * 0.25
         phirdot = MAX_SPEED * -0.1
+
+    # perform odometry
+    delta_x += r * delta_t * (phildot + phirdot) / 2.0
+    delta_w += r * delta_t * (phirdot - phildot) / d
+
+    print (delta_x, delta_w)
 
     # set actuator
     motor_left.setVelocity(phildot)
